28c41a519b5aac75f35d0017f0fe1f3089baa191
Gerardo Torreblanca Luna <gtorreblancaluna@gmail.com>
2022-01-24 08:45:58 -0600

    handle errors, when open new window

--- src/mobiliario/principal.java
+++ src/mobiliario/principal.java
@@ -600,8 +600,12 @@ public class principal extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
-        abrir_nueva_renta();
+        try {
+            abrir_nueva_renta();
+        } catch (PropertyVetoException ex) {
+            JOptionPane.showMessageDialog(null, ex, "Error", JOptionPane.ERROR);
+            Logger.getLogger(principal.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
@@ -609,6 +613,7 @@ public class principal extends javax.swing.JFrame {
             // TODO add your handling code here:
             abrir_consultar_renta();
         } catch (PropertyVetoException ex) {
+            JOptionPane.showMessageDialog(null, ex, "Error", JOptionPane.ERROR);
             Logger.getLogger(principal.class.getName()).log(Level.SEVERE, null, ex);
         }
     }//GEN-LAST:event_jButton4ActionPerformed
